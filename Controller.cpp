197f9f448ab64eeebd8a998b3eee1a8c46efbb7b
Ian Murphy <ian@thanatos.local>
2015-10-27 12:58:16 -0700

    Test

---
+++ Controller.cpp
@@ -0,0 +1,103 @@
+//
+//  Controller.cpp
+//  Sparse Matrix
+//
+//  Created by Ian Murphy on 10/22/15.
+//  Copyright © 2015 Ian Murphy. All rights reserved.
+//
+
+#include "Controller.hpp"
+#include <fstream>
+#include <iostream>
+using std::ifstream;
+
+Controller::Controller()
+{
+    
+}
+
+void Controller::Execute(char * in, char * out)
+{
+    
+    ifstream fin(in);
+    std::ofstream fout(out);
+    
+    if(fin.is_open() && fout.is_open())
+    {
+        bool exit = false;
+        while(!exit && !fin.eof())
+        {
+            
+            fin.getline(m_current_command, 50);
+            GetCommand();
+            exit = SwitchCommand(fout);
+        }
+    }
+    else
+        throw "File cannot be opened";
+    
+}
+
+bool Controller::SwitchCommand(std::ofstream &fout)
+{
+    switch(*arg[0])
+    {
+        case 'I':
+        {
+            Person * temp = new Person(arg[2], arg[3], atoi(arg[1]), atoi(arg[4]), atoi(arg[5]));
+            myIsam.Insert(temp);
+            break;
+        }
+        case 'R':
+        {
+            myIsam.Display(atoi(arg[1]), atoi(arg[2]), fout);
+            break;
+        }
+        case 'E':
+        {
+            return true;
+        }
+        case 'S':
+        {
+            fout<<"\nSearching for "<<arg[1]<<"\n";
+            coords loc = myIsam.Search(arg[1], fout);
+            if(loc.row != -1)
+            {
+                fout<<"Found at ["<<loc.col<<", "<<loc.row<<"]"<<std::endl;
+            }
+            break;
+        }
+        case 'D':
+        {
+            fout<<"\nSearching for "<<arg[1]<<" to delete\n";
+            coords loc = myIsam.Search(arg[1], fout);
+            if(loc.row != -1)
+            {
+                myIsam.Delete(loc);
+                fout<<"Found at ["<<loc.col<<", "<<loc.row<<"] and deleted"<<std::endl;
+            }
+            
+        }
+        default:
+        {
+            //fhaksdfhka
+        }
+            
+    }
+    return false;
+}
+
+void Controller::GetCommand()
+{
+    
+    char * c = strtok(m_current_command, " \r");
+    int i = 0;
+    arg[i] = c;
+    while(c)
+    {
+        i++;
+        c = strtok(NULL, " \r");
+        arg[i] = c;
+    }
+
+}
